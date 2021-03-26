49b1f6b2eb323d43382d3cb1e5c63a17b6443104
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-03-26 19:26:11 +0000

    js file created and all required elements add

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// 1.You will have only <span>10 seconds</span> per each question.
-// 2.Once you select your answer, you can't reselect.
-// 3.You can't select any option once time goes off.
-// 4.You can't exit from the Quiz while you're playing.
-// 5.You'll get points on the basis of your correct answers.
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
