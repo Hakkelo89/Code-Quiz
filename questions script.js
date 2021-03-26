17f6a7c2cec2edf37a4ae8a261c1b134b0010c1e
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-03-26 19:13:57 +0000

    question js file created and first question added

---
+++ questions script.js
@@ -0,0 +1,15 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Istanbul is the world's only major city that sits on two continents. Which continents are they?",
+    answer: "Asia and Europe",
+    options: [
+      "Asia and Africa",
+      "Europe and Africa",
+      "Asia and Europe",
+      "Eurasia and Asia",
+    ],
+  },
+  
