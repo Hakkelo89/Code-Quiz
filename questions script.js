7a3f7c0ed9dfea5c9e86d32087dba0be8eb056e6
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-03-26 19:25:01 +0000

    4 more question add in question js file

--- questions script.js
+++ questions script.js
@@ -12,4 +12,52 @@ let questions = [
       "Eurasia and Asia",
     ],
   },
-  
+  {
+    numb: 2,
+    question: "How much of Istanbul's population lives in Europe?",
+    answer: "Two-thirds",
+    options: ["One-third", "One-half", "Two-thirds", "All of them"],
+  },
+  {
+    numb: 3,
+    question: "Which name has Istanbul NOT been known by?",
+    answer: "Persepolis",
+    options: ["Persepolis", "Byzantium", "Constantinople", "Stanpoli"],
+  },
+  {
+    numb: 4,
+    question:
+      "Established in fourth-century Byzantium, which ecumenical patriarchate remains in modern-day Istanbul?",
+    answer: "Eastern Orthodox Church",
+    options: [
+      "Russian Orthodox Church",
+      "Eastern Orthodox Church",
+      "Byzantine Church",
+      "Greek Orthodox Church",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "The famous domed Istanbul museum Hagia Sophia was originally built as which of the following?",
+    answer: "A Christian Church",
+    options: [
+      "A Christian Church",
+      "An Islamic Mosque",
+      "A School",
+      "A Hospital",
+    ],
+  },
+  // Another question code for the future,
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
